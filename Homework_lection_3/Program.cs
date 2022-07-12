0a051629022d58c29b609dec69d8f5e33a95cfbe
hubert kieslich <hubertargo@interia.pl>
2022-07-12 16:40:53 +0200

    Setting general outline

--- Homework_lection_3/Program.cs
+++ Homework_lection_3/Program.cs
@@ -4,9 +4,29 @@ namespace Homework_lection_3
 {
     class Program
     {
+        public static int factorial_function_with_LINQ(int input)
+        {
+            int score = 0;
+
+            //To Do:
+
+            return score;
+        }
+
+        public static int Fibonacci_sequence_with_LINQ(int input)
+        {
+            int score = 0;
+
+            //To Do:
+
+            return score;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(factorial_function_with_LINQ(5));
+            Console.WriteLine(Fibonacci_sequence_with_LINQ(5));
+
+            Console.ReadKey();
         }
     }
 }
