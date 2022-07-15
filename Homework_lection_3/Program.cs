100620c7eb9c6c72565b6519e1dc18251e883abf
hubert kieslich <hubertargo@interia.pl>
2022-07-15 21:12:08 +0200

    Turned methods into LINQ ones.

--- Homework_lection_3/Program.cs
+++ Homework_lection_3/Program.cs
@@ -8,58 +8,35 @@ namespace Homework_lection_3
     {
         public static int factorial_function_with_LINQ(int input)
         {
-            //To DO:
-            // turn it into LINQ method
-
-            // input - number of factorial 
-
-            List<int> factorials = new List<int>();
-
-            factorials.Add(1);
-            factorials.Add(1);
-            if (input == 0 || input == 1)
-            {
-                return 1;
-            }
-            else
-            {
-                for (int i=2;i<input;i++)
-                {
-                    factorials.Add(factorials[i - 1] * i);
-                }
-                return input * factorial_function_with_LINQ(input - 1);
-            }
+            IEnumerable<int> values = Enumerable.Range(1, input);
+            return values.Aggregate((acumulator,next_value) => acumulator * next_value);          
         }
 
         public static int Fibonacci_sequence_with_LINQ(int input)
         {
-            //To DO:
-            // turn it into LINQ method
 
-            // input - number of fibonaci 
+            return Enumerable.Range(0, input).Skip(1).Aggregate(new { previous = 0, current = 1 }, (previous, _) => new { previous = previous.current, current = previous.previous + previous.current }).current;
 
-            List<int> Fibonacci = new List<int>();
-            Fibonacci.Add(0);
-            Fibonacci.Add(1);
+            /*
+             * It's my failed attempt. Probably didn't work because Method "Where" saw "Index" just as a value and not actual index.
+             * 
+            IEnumerable<int> values = Enumerable.Range(1, input).Select(x => 1);
 
             if (input == 0 || input == 1)
             {
                 return input;
             }
-            else
-            {
-                for (int i = 2; i <= input; i++)
-                {
-                    Fibonacci.Add(Fibonacci[i - 1]+ Fibonacci[i - 2]);
-                }
 
-                return Fibonacci.Last();
-            }   
+            values =values.Where(index => index > 1).Select((index,x) => values.ElementAt(index-2)+ values.ElementAt(index-1));
+            return values.Last();
+            */
         }
         static void Main(string[] args)
         {
-            WriteLine(factorial_function_with_LINQ(5));
-            WriteLine(Fibonacci_sequence_with_LINQ(5));
+            int test_value=5;
+
+            WriteLine(factorial_function_with_LINQ(test_value));
+            WriteLine(Fibonacci_sequence_with_LINQ(test_value));
 
             ReadKey();
         }
